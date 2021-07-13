135cfed78f84e8e68dec3bcb013fef06733a8773
mahafuztechie <mahafuzaadeez5@gmail.com>
2021-07-13 22:34:24 +0530

    completed

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({ results }) {
   return (
     <div>
       <Head>
-        <title>Hulu2</title>
+        <title>Hulu-clone</title>
       </Head>
 
       {/* Header */}
